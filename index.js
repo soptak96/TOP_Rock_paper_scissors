a1b8c53e9953d03d2985021ef7af717297dd2e44
soptak96 <144949460+soptak96@users.noreply.github.com>
2023-10-06 00:12:38 +0600

    Add getComputerChice function for generate random choices.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+// console.log("Hello world!")
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 3)
+    if(randNum === 0){
+        return "ROCK"
+    } else if (randNum === 1) {
+        return "PAPER"
+    } else {return "SCISSOR"}
+}
+console.log(getComputerChoice())
