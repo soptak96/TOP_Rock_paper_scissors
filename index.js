91e7300f44d610d930310c6c4015d2fcf47faec8
soptak96 <144949460+soptak96@users.noreply.github.com>
2023-10-06 13:10:45 +0600

    Add a function that plays a single round.

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ function getComputerChoice() {
         return "ROCK"
     } else if (randNum === 1) {
         return "PAPER"
-    } else {return "SCISSOR"}
+    } else {return "SCISSORS"}
 }
-console.log(getComputerChoice())
+
+const computerSelection = getComputerChoice();
+
+const playerSelection = "rock".toUpperCase();
+
+
+console.log(playerSelection)
+
+
+console.log(computerSelection)
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "ROCK" && computerSelection === "PAPER"){
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "You Win! Scissors beats Paper"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "You Lose! Rock beats Scissors"
+    } else return "Draw"
+  }
+
+  console.log(playRound(playerSelection, computerSelection))
