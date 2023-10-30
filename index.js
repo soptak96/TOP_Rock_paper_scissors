18afcbd5ddd8581994e7eff4b0b02fca09491e86
soptak96 <144949460+soptak96@users.noreply.github.com>
2023-10-30 19:19:02 +0600

    give the rps game a UI

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log("Hello world!")
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3)
     if(randNum === 0){
@@ -13,58 +12,154 @@ let playerScore = 0;
 
 let computerScore = 0;
 
-function game() {
-    
- for(let i = 1; i <= 5; i++){
-        let playerSelection = window.prompt("enter your choice", "").toUpperCase();
-        let computerSelection = getComputerChoice();
+let finalResult = "";
 
-        function playRound(playerSelection, computerSelection) {
-            if(playerSelection === "ROCK" && computerSelection === "PAPER"){
-                return "You Lose! Paper beats Rock"
-            } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-                return "You Win! Rock beats Scissors"
-            } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-                return "You Lose! Scissors beats Paper"
-            } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-                return "You Win! Paper beats Rock"
-            } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-                return "You Win! Scissors beats Paper"
-            } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-                return "You Lose! Rock beats Scissors"
-            } else return "Draw"
-        }
+let displayChoiceVar = document.querySelector
 
-        let roundResult = playRound(playerSelection, computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "ROCK" && computerSelection === "PAPER"){
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "You Win! Scissors beats Paper"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "You Lose! Rock beats Scissors"
+    } else return "Draw"
+}
+
+
+function displayChoice() {
+    console.log(getComputerChoice())
+}
+
+
+function updateScore(roundResult) {
     
         if(roundResult.match("W")){
             playerScore ++;
         
         } else if(roundResult.match("L")) {
-            computerScore ++;
-            
-        }
-        console.log(playerSelection);
-
-        console.log(computerSelection);
-
-        console.log(roundResult);
-
-        console.log(`Player Score: ${playerScore};
-Computer Score: ${computerScore}`);
-    }
-
-    let finalResult = "";
-    if(playerScore > computerScore) {
-        finalResult = "final result: Congratulations!"
-    } else if(playerScore < computerScore) {
-        finalResult = "final result: Sorry!"
-    }
-    else {finalResult = "final result: Draw!"}
-
-    console.log(finalResult);
+            computerScore ++;}
 
 }
 
+function change_color(computerSelection) {
+    // Change the color of the visual element based on the choice
+    if (computerSelection === 'ROCK') {
+        // Change the color of visual element 1
+        rock_C.style.backgroundColor = 'yellow';
+        paper_C.style.backgroundColor = '';
+        scissors_C.style.backgroundColor = '';
+    } else if (computerSelection === 'PAPER') {
+        // Change the color of visual element 2
+        rock_C.style.backgroundColor = '';
+        paper_C.style.backgroundColor = 'yellow';
+        scissors_C.style.backgroundColor = '';
+    } else if (computerSelection === 'SCISSORS') {
+        // Change the color of visual element 3
+        rock_C.style.backgroundColor = '';
+        paper_C.style.backgroundColor = '';
+        scissors_C.style.backgroundColor = 'yellow';
+    }
+}
 
-game()
+    function decideWinner(){
+        if(playerScore === 5) {
+            finalResult = "Congratulations! You won the game"
+            winner.textContent = finalResult;
+          
+        } else if(computerScore === 5) {
+            finalResult = "Sorry! You lost the game"
+            winner.textContent = finalResult;
+           
+        }
+
+    }
+
+    function gameEnd() {
+        if(finalResult.length > 0){
+            game.style.display = "none";
+            resultDiv.classList.remove("resultGame");
+            resultDiv.classList.add("resultDisplay")
+            // resultDiv.style.d;
+           
+        }
+    }
+
+    function restart() {
+        restartBtn.addEventListener("click", () =>
+        window.location.reload())
+    }
+
+        const rock = document.querySelector("#rock");
+        const paper = document.querySelector("#paper");
+        const scissors = document.querySelector("#scissors");
+        const rock_C = document.querySelector("#rock_C");
+        const paper_C = document.querySelector("#paper_C");
+        const scissors_C = document.querySelector("#scissors_C");
+        const yourScore = document.querySelector("#your-score");
+        const machineScore = document.querySelector("#computer-score")
+        const winner = document.querySelector("h3");
+        const resultDiv = document.querySelector("#result");
+        yourScore.textContent = playerScore;
+        machineScore.textContent = computerScore;
+        winner.textContent= finalResult;
+        const playerChoiceBtn = [rock,paper,scissors]
+        const restartBtn = document.querySelector("#reload");
+        let selectedButtons = null;
+        const info = document.querySelector("#info")
+        const game = document.querySelector("#game")
+       
+        
+        
+        
+        playerChoiceBtn.forEach(btn => {
+         btn.addEventListener("click",function(event) {
+            
+            let playerSelection = this.innerText.toUpperCase();
+
+            let computerSelection = getComputerChoice();
+
+            let roundResult = playRound(playerSelection, computerSelection);
+
+            if(selectedButtons){
+                selectedButtons.style.backgroundColor = "aliceblue"
+            }
+            this.style.backgroundColor = "red";
+            selectedButtons = this;
+
+            info.textContent = roundResult;
+        
+            updateScore(roundResult);
+
+            change_color(computerSelection)
+
+            yourScore.textContent = playerScore;
+            machineScore.textContent = computerScore;
+
+            decideWinner();
+
+            gameEnd();
+
+            restart();
+        
+            
+            console.log(computerSelection);
+            console.log(playerSelection)
+         
+
+           console.log(`Player Score: ${playerScore};
+Computer Score: ${computerScore}`);
+        });
+    })
+
+    
+
+
+
